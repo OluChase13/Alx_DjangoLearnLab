6af6c24d5c89240048372b367b3be22ca5879fe4
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-11-11 21:56:26 +0100

    views created for login, logout and reg

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
-from .views import list_books
+from .views import list_books, CustomLoginView, CustomLogoutView, register
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+]
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+    # Add other URLs for your app here
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+
 
 # Function-based view for listing all books
 def list_books(request):
@@ -13,3 +18,23 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Login view
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+# Logout view
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
+
+# Register view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to homepage after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
