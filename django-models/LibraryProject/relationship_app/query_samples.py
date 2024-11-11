9825393b2c76e1b4b4e3b41e0b88ef2231d991f2
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-11-11 17:16:42 +0100

    text

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        return author.books.all()  # Returns all books related to the author
+        return Book.objects.filter(author=author)
     except Author.DoesNotExist:
         return None
 
