71505333ea9a9d84969b203b2be27ad6ad45e7a8
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-11-12 23:07:12 +0100

    admin view added

--- django-models/relationship1_app/query_samples.py
+++ django-models/relationship1_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        return Book.objects.filter(author=author)
+        return author.books.all()  # Returns all books related to the author
     except Author.DoesNotExist:
         return None
 
@@ -21,7 +21,7 @@ def get_books_in_library(library_name):
 # Retrieve the librarian for a specific library
 def get_librarian_for_library(library_name):
     try:
-        library = Librarian.objects.get(library=library_name)
-        return Librarian.library  # Returns the librarian related to the library
+        library = Library.objects.get(name=library_name)
+        return library.librarian  # Returns the librarian related to the library
     except Library.DoesNotExist:
         return None
---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from .views import list_books, CustomLoginView, CustomLogoutView, register
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    path('login/', CustomLoginView.as_view(), template_name='login'),
+    path('logout/', CustomLogoutView.as_view(), template_name='logout'),
+    path('register/', register, name='register'), 
+    path(views.register, LogoutView.as_view(template_name='logout' ), LoginView.as_view(template_name='login'),
+    # Add other URLs for your app here
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
