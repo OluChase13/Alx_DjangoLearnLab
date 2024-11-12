340b08d77bee6b4ffe56b7b3aeb68c3766339cb5
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-11-12 23:10:10 +0100

    admin view added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -45,7 +45,7 @@ def register(request):
 
 # Helper functions to check user role
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
