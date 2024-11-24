4a26cecbe66d7fe7e5d5cb7666211ac3102ab97c
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-11-24 12:13:55 +0100

    CRUD operations implemented with ViewSets and Routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.generics import ListAPIView
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -12,3 +10,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
