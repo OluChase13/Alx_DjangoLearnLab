5cb29283ae6734e8227c33774d0a0fa6ed772eda
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-11-24 11:40:19 +0100

    API Endpoint built

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import generics
 from rest_framework.generics import ListAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -7,8 +8,7 @@ from  .serializers import BookSerializer
 
 # Create your views here.
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    
