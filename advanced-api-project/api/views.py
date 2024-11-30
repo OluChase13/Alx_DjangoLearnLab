3ed248f262afdc1d1a680fcfeaa1f832eece2e78
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-11-30 15:29:49 +0100

    custom views and generic views built

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, serializers
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class BookListView(generics.ListAPIView):
