a78657ae2a70729f295dc047ff13ead219d5b782
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-12-07 09:18:50 +0100

    Blog post management features created

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -18,3 +18,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
